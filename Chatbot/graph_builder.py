c17e68c6cb50b63425e35a4b03f58cf55866bc4a
ritika24-s <24octritika@gmail.com>
2025-06-29 16:44:34 +0100

    add react agent architecture

--- Chatbot/graph_builder.py
+++ Chatbot/graph_builder.py
@@ -34,6 +34,7 @@ def run_tool_calling_llm():
         # If the latest message (result) from assistant is a not a tool call -> tools_condition routes to END
         tools_condition
     )
+    graph_builder.add_end_edge("tools")
 
     # compile graph
     graph = graph_builder.compile_graph()
@@ -42,4 +43,28 @@ def run_tool_calling_llm():
     result = graph_builder.get_result(data=data)
 
 
+def react_agent_architecture():
+    def run_tool_calling_llm():
+    tools = get_tools()
 
+    # Graph builder
+    graph_builder = ChatbotGraphBuilder()
+    # Add nodes
+    graph_builder.add_node("tool_calling_llm", tools_node)
+    graph_builder.add_node("tools", ToolNode(tools))
+
+    # Add edges
+    graph_builder.add_start_edge("tool_calling_llm")
+    graph_builder.add_condition(
+        "tool_calling_llm",
+        # If the latest message (result) from assistant is a tool call -> tools_condition routes to tools
+        # If the latest message (result) from assistant is a not a tool call -> tools_condition routes to END
+        tools_condition
+    )
+    graph_builder.add_edge("tools","tool_calling_llm")
+
+    # compile graph
+    graph = graph_builder.compile_graph()
+    graph_builder.visualize_graph()
+    data = {"messages":"what is 5 multiplied by 2?"}
+    result = graph_builder.get_result(data=data)
